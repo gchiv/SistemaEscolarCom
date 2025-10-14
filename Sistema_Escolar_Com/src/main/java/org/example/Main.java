8218fdcb44f1c99d8d8cfe91df03560c37089cca
CastilloDevX <al075041@uacam.mx>
2025-10-14 11:40:58 -0600

    feat: :rocket: initial proyect with Main.java Structure with SSH Config & pom with MariaDB dependencies added

--- Sistema_Escolar_Com/src/main/java/org/example/Main.java
+++ Sistema_Escolar_Com/src/main/java/org/example/Main.java
@@ -1,17 +1,49 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.sql.*;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    public static void main(String []args) throws JSchException {
+        String hostname = "fi.jcaguilar.dev";
+        String sshUser = "patito";
+        String sshPass = "cuack";
+
+        String dbUser = "becario";
+        String dbPass = "FdI-its-5a";
+
+        JSch jsch = new JSch();
+        // ssh patito@fi.jcaguilar.dev => Equivalencia en la terminal
+        Session session = jsch.getSession(sshUser, hostname);
+        session.setPassword(sshPass); // introducir la contraseña
+
+        // Deshabilita los mensajes de error
+        session.setConfig("StrictHostKeyChecking", "no");
+
+        // Obtenemos un puerto redireccional
+        session.connect();
+
+        int port = session.setPortForwardingL(0, "localhost", 3306);
+        String conString = "jdbc:mariadb://localhost:"+ port +"/its5a";
+        System.out.println(conString);
+
+        try (Connection conexion = DriverManager.getConnection(conString, dbUser, dbPass);
+             Statement stmt = conexion.createStatement();
+             ResultSet rs = stmt.executeQuery("SELECT * FROM personas_escuela")) {
+
+            System.out.println("Conectado correctamente a la base de datos");
+            while (rs.next()) {
+                String nombre = rs.getString(1);
+                String apellido = rs.getString(2);
+                System.out.println(nombre + " " + apellido);
+            }
+        } catch (SQLException e) {
+            System.out.println("Error SQL: " + e.getMessage());
         }
+
+        //Exit
+        session.disconnect();
     }
 }
